201f63feb6ce8bea320c3c5d8577c271300c7bcc
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-13 22:31:32 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,16 +1,27 @@
+<?php
+if(isset($_POST['submit'])){
+    echo "eiei";
+}else{
+    echo "555+";
+}
+?>
 <html>
     <head>
-
+        <style>
+            textarea{
+                resize:none;
+            }
+        </style>
     </head>
     <body>
-    <form method = "post" id="CommentForm" >
+    <form method = "POST" id="CommentForm" >
     Name:<br>
-    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+    <input type="text" name = "name" placeholder="Enter Name"> <br>
     Comment:<br>
-    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+    <textarea rows="10" cols="20" name = "comment" placeholder="Enter Comment"></textarea><br>  
     Link:<br>
-    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
-    <input type="submit" id="commentBtn">
+    <input type="text" name = "link"placeholder="Enter Link"> <br><br>
+    <input type="submit" name="submit">
   </form> 
     </body>
     

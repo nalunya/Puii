af8bb3784270809092c636dc730ce483c59d4e45
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-13 23:48:19 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -17,7 +17,8 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "<script>alert('New record created successfully')";
+    echo "<script>window.location='show.php'</script>;
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }

22d117ce3f83dc4334fa88042cd168d2e00337b3
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-13 21:59:27 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,12 @@
 <?php
-    include("connect.php");
+   
+   $conn = mysqli_init();
+   mysqli_real_connect($conn, 'nalunya.mysql.database.azure.com', 'fa240145@nalunya', 'Puii240145', 'itflab', 3306);
+   if (mysqli_connect_errno($conn))
+   {
+       die('Failed to connect to MySQL: '.mysqli_connect_error());
+   }
+   
     $id=$_GET['id'];
    
     $sql="DELETE * FROM guestbook WHERE ID='$id'";

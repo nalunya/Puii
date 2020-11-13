0d0b1200f561222fe71e178d75ac1eb2db05e484
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-13 22:54:44 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -4,7 +4,7 @@
  $sql = "SELECT FROM guestbook WHERE ID = '".$id."'";
  $result=mysqli_query($conn,$sql);
 if(mysqli_num_rows($result) > 0){
-    $res = $result['Name'];
+    $res = mysqli_fetch_array($result);
 }
  echo $id;
 ?>

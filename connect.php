bf452bc839aecfd47f7164e4748de5fdb724bf5c
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-13 21:10:11 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,6 @@
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'nalunya.mysql.database.azure.com', 'fa240145@nalunya', 'Puii240145', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}

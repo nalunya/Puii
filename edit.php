5a3ee0187fbeaa8292f32878ded6f69118aa5bda
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-13 22:25:47 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+    <form method = "post" id="CommentForm" >
+    Name:<br>
+    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+    Comment:<br>
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+    Link:<br>
+    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
+    <input type="submit" id="commentBtn">
+  </form> 
+    </body>
+    
+</html>

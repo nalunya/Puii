3ecada9c4cd82692c49eb36dfca8ee40ebabd1f0
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-13 21:44:44 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,17 @@
 <?php
     include("connect.php");
     $id=$_GET['id'];
+    echo $id;
+    /*
     $sql="DELETE * FROM `guestbook` WHERE `ID` = '".$id."'";
     $result=mysqli_query($conn,$sql);
+    
     if($result){
         echo "<script>alert('ลบเสร็จสิ้น')</script>";
         echo "<script>window.location='show.php'</script>";
     }else{
-        echo "error";
+        echo "<script>alert('error')</script>";
+        echo "<script>window.location='show.php'</script>";
     }
-
+    */
 ?>

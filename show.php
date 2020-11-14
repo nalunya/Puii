bea3b3102e1fc67afa23fb8e483da06a4220aeb4
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-14 12:29:00 +0700

    Update show.php

--- show.php
+++ show.php
@@ -2,10 +2,13 @@
 <head>
 <title>ITF Lab</title>
 <style>
-  
+  body{
+    background:black;
+  }
   table{
     color:#fff;
     border:1px solid #fff;
+    padding:10px;
   }
 </style>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">

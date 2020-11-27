30214c1f9a07d7bfac5ec44e77391cc3e65307e5
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-27 17:47:25 +0700

    Update process.php

--- process.php
+++ process.php
@@ -5,11 +5,11 @@
 $height1=$height/100;
   $bmi=$weight/($height1*$height1);
  $sql="INSERT INTO newbook (name,height, weight,bmi)
-VALUES ('".$name."', '".$height."', '".$weight."','".bmi."')";
+VALUES ('".$name."', '".$height."', '".$weight."','".$bmi."')";
  $result=mysqli_query($conn,$sql);
  if($result){
    echo "<script>alert('เพิ่มข้อมูลสำเร็จ')<script>";
-   echo "<script>window.location='show.php'<script>";
+   echo "<script>window.location='showw.php'<script>";
 }else{
 echo "<script>alert('Error')<script>";
 echo "<script>window.location='insert.html'<script>";

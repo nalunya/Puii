bbd0eabc27ba517744f7b62abea1c3709089a759
nalunya <73693660+nalunya@users.noreply.github.com>
2020-11-27 17:07:25 +0700

    Update process.php

--- process.php
+++ process.php
@@ -2,11 +2,8 @@
  include('connect.php');
  $name=$_POST['name'];
  $height=$_POST['height'];     $weight=$_POST['weight'];
-
-$height1=number_format($height/100,2);
-$bmi=number_format($weight/($height1*$height1),2);
- $sql="INSERT INTO newbook (name,height, weight,bmi)
-VALUES ('".$name."', '".$height."', '".$weight."','".$bmi."')";
+ $sql="INSERT INTO newbook (name,height, weight)
+VALUES ('".$name."', '".$height."', '".$weight."')";
  $result=mysqli_query($conn,$sql);
  if($result){
    echo "<script>alert('เพิ่มข้อมูลสำเร็จ')<script>";
